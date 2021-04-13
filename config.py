bef92b0d8cf3fc8eadb764168e73acf426c87eaf
chetat <yekuwilfred@gmail.com>
2021-04-13 10:04:47 +0100

    Update config file

--- config.py
+++ config.py
@@ -118,9 +118,9 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
     DEBUG = False
     USE_RELOADER = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
-        'sqlite:///' + os.path.join(basedir, 'data.sqlite'))
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SSL_DISABLE = (os.environ.get('SSL_DISABLE', 'True') == 'True')
+    print(SQLALCHEMY_DATABASE_URI)
 
     @classmethod
     def init_app(cls, app):
