b59773b055b5159b99be10a0268d9a6b5d01e24c
chetat <yekuwilfred@gmail.com>
2021-04-13 10:21:31 +0100

    Update config

--- manage.py
+++ manage.py
@@ -11,6 +11,7 @@ from app import create_app, db
 from app.models import Role, User
 from config import Config
 
+print(os.getenv('FLASK_CONFIG'))
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
