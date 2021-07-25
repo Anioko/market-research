70806a0010cfd87e757a3a4976496c8fd7052a00
anioko1 <aniekanokono@gmail.com>
2021-07-25 13:44:38 +0100

    admin.py & temp.html

--- app/models/admin.py
+++ app/models/admin.py
@@ -5,4 +5,4 @@ class TrackingScript(db.Model):
     __tablename__ = "tracking_script"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    script = db.Column(db.String(150), nullable=False)
+    script = db.Column(db.String(500), nullable=False)
